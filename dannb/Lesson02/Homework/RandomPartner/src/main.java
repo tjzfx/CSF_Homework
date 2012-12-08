803b3fa1ae548d1d5ff723c5acf8bc7d60f0da94
DannBerg <dann@dann-berg.comgit credential-osxkeychaingit config --global user.email dann@dann-berg.com>
2012-12-08 18:35:55 -0500

    Add loop to match students if total number of students is odd.

--- dannb/Lesson02/Homework/RandomPartner/src/main.java
+++ dannb/Lesson02/Homework/RandomPartner/src/main.java
@@ -39,7 +39,23 @@ public class main {
 				}
 			}
 		} else {
-			
+			while (numberOfStudents - matchedStudents.size() > 3) {
+				// Pick random student by generating random int between 0 and numberOfStudents
+				int student1 = (int) (Math.random() * numberOfStudents);
+				int student2 = (int) (Math.random() * numberOfStudents);
+				// Make sure the two students aren't the same and that they haven't been assigned yet
+				if (student1 != student2 && !matchedStudents.contains(student1) && !matchedStudents.contains(student2)) {
+					// Add students to matchedStudents array that stores students who have already been matched
+					matchedStudents.add(student1);
+					matchedStudents.add(student2);
+					System.out.println(listOfStudents[student1] + ", " + listOfStudents[student2]);
+				}
+			}
+			int student1 = (int) (Math.random() * numberOfStudents);
+			int student2 = (int) (Math.random() * numberOfStudents);
+			int student3 = (int) (Math.random() * numberOfStudents);
+			if (student1 != student2 && student1 != student3 && student2 != student3 && !matchedStudents.contains(student1) && !matchedStudents.contains(student2) && !matchedStudents.contains(student3)) {
+				System.out.println(listOfStudents[student1] + ", " + listOfStudents[student2] + ", " + listOfStudents[student3]);
 		}
 	
 	}
