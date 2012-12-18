199ae03dbb663ce1b5f99378b196f3490c92ce08
Cody Duval <cody.duval@gmail.com>
2012-12-18 13:52:52 -0500

    Finished week 4 homework

--- codyd/Lesson3/src/IntLinkedList.java
+++ codyd/Lesson3/src/IntLinkedList.java
@@ -2,13 +2,14 @@
 public class IntLinkedList {
     // When a new IntLinkedList is created, Java makes firstNode == null by default
     public IntListNode firstNode;
-
+    int counter = 0;
     // Add the integer, or take no action if it's already in the list
     public void insert(int value) {
         if (firstNode == null) {
             // This was an empty, newly created list, so set the first node and its value
             firstNode = new IntListNode();
             firstNode.value = value;
+            incrementCount();
         }
         else {
             // There was a first node, so find the 'terminal' node,
@@ -19,6 +20,7 @@ public class IntLinkedList {
                     IntListNode nextNode = new IntListNode();
                     nextNode.value = value;
                     node.nextNode = nextNode;
+                    incrementCount();
                 }
 
                 node = node.nextNode;
@@ -32,7 +34,12 @@ public class IntLinkedList {
         // and print out the value in each, from first to last
         // Order for the print-out of list containing 1, 2, 3, 4, 5, 6: "1 2 3 4 5 6"
         IntListNode node = firstNode;
+        System.out.println("There are " + showCount() + " numbers in the list.");
         while (true) {
+        	if (node==null) {
+        		System.out.print("Empty list!");
+        		break;
+        	}
         	System.out.print(" " + node.value);
         	if (node.nextNode != null) {
         		node = node.nextNode;
@@ -47,30 +54,61 @@ public class IntLinkedList {
     public void remove(int value) {
         // Remove the node that contains this value from the list
         // If no such node exists in the list, no action should be taken
-//    	IntListNode node = firstNode;
-//    	while(true) {
-//	    	if (node.value == value) {
-//	    		node = node.nextNode;
-//	    		node.next
-//	    	}
-//	    	else {
-//	    		node = node.nextNode
-//	    	}
-//    	}
-    	
-        System.out.println("Remove Not Implemented");
+    	IntListNode node = firstNode;
+    	IntListNode previousNode = firstNode;
+    	while(node != null) {
+    		if (node == previousNode && node.value == value){
+    			firstNode = node.nextNode;
+    			node.nextNode = null;
+    			decrementCount();
+    			break;
+    		}
+	    	if (node != null && node.value == value) {
+	    		previousNode.nextNode = node.nextNode;
+	    		node.nextNode = null;
+	    		decrementCount();
+	    		break;
+	    	}
+	    	else {
+	    		previousNode = node;
+	    		if (node.nextNode == null) {
+	    			System.out.println("Sorry, I can't find that number in the list!");
+	    			System.out.print("The numbers in the list are: ");
+	    			break;
+	    		}
+	    		node = node.nextNode;
+	    		
+	    	}
+    	}
     }
 
     // TODO: Optional Challenge HW Question for next Tuesday:
-    public int count() {
+    public int showCount() {
+    	return counter;
         // This method should return an int with the current count of objects in the list
         // Leave a comment with the Big-O complexity of this method
-        // Hint: With a little extra work it can be done in O(1) time
-        return 0;
+        // This counter is O(1) in complexity - it fires once every time a number is entered.
+    }
+    
+    public void incrementCount() {
+    	counter++;
+    }
+    
+    public void decrementCount() {
+    	counter--;
     }
 
     // TODO: Optional Challenge HW Question for next Tuesday:
     public void reverse() {
+    	IntListNode node = firstNode;
+    	firstNode = null;
+    	while (node != null) {
+    		IntListNode temp = node;
+    		node = temp.nextNode;
+    		temp.nextNode = firstNode;
+    		firstNode = temp;
+    	}
+    	return;
         // This method should completely reverse the linked list
         // Leave a comment with the Big-O complexity of this method
         // (note: extremely popular interview question)
--- codyd/Lesson3/src/LinkedListPractice.java
+++ codyd/Lesson3/src/LinkedListPractice.java
@@ -4,10 +4,13 @@ public class LinkedListPractice {
 
     public static void main(String[] args) {
         Scanner consoleScanner = new Scanner(System.in);
-        System.out.print("\nEnter 'list' to start: ");
+        System.out.print("\nEnter 'list or stack' to start: ");
         String command = consoleScanner.next();
         if (command.equals("list")) {
             linkedList();
+        }
+        else if(command.equals("stack")) {
+            intStack();
         }
         else {
             System.out.println("Invalid input, bailing");
@@ -29,8 +32,41 @@ public class LinkedListPractice {
             else if (command.equals("remove")) {
                 list.remove(value);
             }
+            else if (command.equals("reverse")) {
+                list.reverse();
+            }
 
             list.print();
+        }
+    }
+    
+    public static void intStack() {
+    	IntStack list =  new IntStack();
+
+        while(true) {
+            Scanner consoleScanner = new Scanner(System.in);
+            System.out.print("\nEnter {push/pop/size/quit} to modify the list: ");
+            String command = consoleScanner.next();
+            
+            if (command.equals("push")) {
+            	System.out.print("\nEnter number to push onto list: ");
+            	int value = consoleScanner.nextInt();
+                list.push(value);
+            }
+            else if (command.equals("pop")) {
+                int x = list.pop();
+                System.out.println("The top of the stack is now the integer " + x);
+            }
+            else if (command.equals("size")) {
+                int size = list.size();
+                System.out.println(size);
+            	
+            }
+            else {
+            	System.out.print("Goodbye!");
+            	break;
+            	
+            }
         }
     }
 }
