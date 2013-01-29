f1f4002049154b6df583cd3e5157d701472bb0eb
Rudd T <rudd@varka.la>
2013-01-29 14:38:26 -0500

    lesson 13 sample code

---
+++ ruddt/Lesson13/CountToOneMillion.java
@@ -0,0 +1,12 @@
+package com.generalassembly;
+
+public class CountToOneMillion implements Runnable {
+	@Override
+	public void run() {
+		int sum = 0;
+		for (int i = 0; i < 1E6; i++) {
+			sum++;
+		}
+		System.out.println(sum);
+	}
+} 
