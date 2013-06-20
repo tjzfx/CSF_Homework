d51c6b17231ea2675de1f90e82dbf6d1a7f50472
Rudd T <rudd@varka.la>
2013-06-20 11:11:18 -0400

    lesson 01 class exercises

---
+++ Lesson01/src/Lesson01Samples.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Lesson01Samples {
+    public static void main(String[] args) {
+        // Use this to test FibPrinter and Lesson01Samples
+
+        HelloWorldPrinter printer = new HelloWorldPrinter();
+        printer.helloWorld();
+
+        // Uncomment to test fibPrinter
+        // FibPrinter printer = new FibPrinter();
+        // printer.askForNumberAtPosition();
+    }
+}
