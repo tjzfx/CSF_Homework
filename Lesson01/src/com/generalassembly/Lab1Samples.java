b9de1e3db4f3150a652db4d55dc30bb6c8eb4fa8
Rudd T <rudd@varka.la>
2013-12-09 15:38:15 -0500

    example solutions for lesson 01

--- Lesson01/src/com/generalassembly/Lab1Samples.java
+++ Lesson01/src/com/generalassembly/Lab1Samples.java
@@ -1,14 +1,18 @@
-package com.generalassembly;
-
 import java.util.Scanner;
 
 public class Lab1Samples {
 
+    static final int DRIVING_AGE = 16;
+    static final int VOTING_AGE = 18;
+    static final int DRINKING_AGE = 21;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Your name: ");
-        String name = scanner.next();
-        System.out.println("Hello " + name);
+//        helloWorld();
+//        helloMultipleTimes();
+//        helloWorldWithStackedRights();
+        helloWorldWithComboRights();
+//        helloWorldWithSeparateClass();;
+//        oddEven();
     }
 
     public static void helloWorld() {
@@ -22,10 +26,27 @@ public class Lab1Samples {
     }
 
     public static void helloWorldWithStackedRights() {
+
         // TODO: Print the following
         // If your age is above 16, print "You can drive!"
         // If your age is above 18, print "You can vote!"
         // If your age is above 21, print "You can drink!"
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Your name: ");
+        String name = scanner.next();
+
+        System.out.print("\nYour age: ");
+        int age = scanner.nextInt();
+
+        if (age >= DRIVING_AGE) {
+            System.out.println("You can drive!");
+        }
+        if (age >= VOTING_AGE) {
+            System.out.println("You can vote!");
+        }
+        if (age >= DRINKING_AGE) {
+            System.out.println("You can drink!");
+        }
     }
 
     public static void helloWorldWithComboRights() {
@@ -33,21 +54,41 @@ public class Lab1Samples {
         // If your age is above 16 but below 18, print "You can drive!"
         // If your age is above 18 but below 21, print "You can drive and vote!"
         // If your age is above 21, print "You can drive, vote and drink (but not at the same time!)"
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Your name: ");
+        String name = scanner.next();
+
+        System.out.print("\nYour age: ");
+        int age = scanner.nextInt();
+
+        if (age >= DRINKING_AGE) {
+            System.out.println("You can drive, vote and drink (but not at the same time!)");
+        }
+        else if (age >= VOTING_AGE) {
+            System.out.println("You can drive and vote!");
+        }
+        else if (age >= DRIVING_AGE) {
+            System.out.println("You can drive!");
+        }
     }
 
     public static void helloMultipleTimes() {
         System.out.println("Enter a number of times to say hello! ");
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        System.out.println("Hello!");
-        // TODO: Modify this to print "Hello!" exactly n times
+        // Modify this to print "Hello!" exactly n times
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Hello!");
+        }
     }
 
     public static void numberAdder() {
         int sum = 0;
         Scanner scanner = new Scanner(System.in);
-        // TODO: Change this to stop execution and print "Total sum : " plus the value when the value exceeds 100
-        while (true) {
+        // Change this to stop execution and print "Total sum : " plus the value when the value exceeds 100
+        while (sum < 100) {
             System.out.print("Enter a number: ");
             int n = scanner.nextInt();
             sum = sum + n;
@@ -61,22 +102,22 @@ public class Lab1Samples {
         int n = scanner.nextInt();
         boolean even = isEven(n);
 
-        // TODO: Print if the number is even or not
+        System.out.println(even ? "Even!" : "Odd!");
     }
 
     public static boolean isEven(int n) {
-        // TODO: Return whether the number is even or odd
-        return false;
+        return n % 2 == 0;
     }
 
     public static void helloWorldWithSeparateClass() {
     /*
-    TODO:
     - Create a HelloWorldPrinter class
     - Create a method in that class, public void sayHello(). It should do the same thing as helloWorld() above
     - Create an instance of that class here
     - Call the method sayHello() on that instance
      */
+        HelloWorldPrinter hello = new HelloWorldPrinter();
+        hello.sayHello();
     }
 
     public static void fib() {
