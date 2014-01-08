6819a8077797404118927b9971cefbcd3c6aae72
Rudd T <rudd@varka.la>
2014-01-08 17:38:35 -0500

    lesson 07

---
+++ Lesson07/src/IntStack.java
@@ -0,0 +1,21 @@
+
+public class IntStack {
+    IntListNode firstNode;
+
+    public IntStack() {
+    }
+
+    public void push(int value) {
+        // TODO: Add a value to the top of the stack
+    }
+
+    public int pop() {
+        // TODO: Pop the first value off the statck
+        return 0;
+    }
+
+    public int size() {
+        // TODO: Fill out this method
+        return 0;
+    }
+}
